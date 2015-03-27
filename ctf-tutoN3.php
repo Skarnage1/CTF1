615aef39b12f7303daff67948d092ddb4d87eff4
noghost13 <as@u8ik.com>
2015-03-27 14:57:34 +0100

    Create ctf-tutoN3.php

---
+++ ctf-tutoN3.php
@@ -0,0 +1,55 @@
+<html>
+
+
+	<head>
+
+	<title>Atelier CTF User agent</title>
+	
+	<link href="css/styleCTF.css" rel="stylesheet" type="text/css">
+
+	</head>
+
+	<body>
+
+		<div id="main">
+		
+		
+			<div id="texte">
+			
+			<h1>CTF</h1>
+			
+			<p>
+			Faites vous passer pour un smartphone... et obtenez le FLAG...
+			</p>
+			
+			</div>
+			<div id="contenu">
+						
+						<ul id="nav-box">
+							<li><a href="index.php">Tuto 1</a></li>
+							<li><a href="ctf-tutoN2.php">Tuto 2</a></li>
+							<li><a href="ctf-tutoN3.php">Tuto 3</a></li>
+							<li><a href="ctf-tutoN4.php">Tuto 4</a></li>
+						</ul>
+			
+				<div id="centre">
+						
+						<p>
+							<img src="./images/android.jpg">
+						</p>
+						
+				</div>
+				
+				<div id="droite">
+					<p>
+						<a href="post_ctf_3.php">...le flag ici...</a>
+					</p>
+				</div>
+				
+				
+			</div>		
+		</div>
+
+	</body>
+
+</html>
