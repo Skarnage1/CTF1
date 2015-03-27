c30b503e646ba279bdb2550a227f2e0e697df063
noghost13 <as@u8ik.com>
2015-03-27 14:56:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+
+
+	<head>
+
+	<title>Atelier CTF 1</title>
+	
+	<link href="css/styleCTF.css" rel="stylesheet" type="text/css">
+
+	</head>
+
+	<body>
+
+		<div id="main">
+		
+		
+			<div id="texte">
+			
+			<h1>CTF</h1>
+			
+			<p>
+			voici un petit tutoriel sur les requetes HTTP... visitez les dossiers
+			du serveur pour obtenir le flag...
+			</p>
+			
+			</div>
+			<div id="contenu">
+						
+						<ul id="nav-box">
+							<li><a href="index.php">Tuto 1</a></li>
+							<li><a href="ctf-tutoN2.php">Tuto 2</a></li>
+							<li><a href="ctf-tutoN3.php">Tuto 3</a></li>
+							<li><a href="ctf-tutoN4.php">Tuto 4</a></li>
+						</ul>
+			
+				<div id="centre">
+						
+						<p>
+							<img src="./images/ctf2.png">
+						</p>
+						
+				</div>
+				
+				<div id="droite">
+					<p>entrez le mot de passe ici :
+					<form method=post action="post_ctf_1.php">
+					<input name="mot_de_passe" type="text" size="14"><br>
+					<INPUT TYPE=SUBMIT VALUE="GO">
+					</form>
+					</p>
+				</div>
+				
+				
+			</div>		
+		</div>
+
+	</body>
+
+</html>
